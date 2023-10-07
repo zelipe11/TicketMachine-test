5e40ae4c137f4595228dfecd2a3a7739643d4bee
JOSE FELIPE DE CASTRO MARTINS <unifjmartins>
2023-10-06 21:30:53 -0300

    Clone de projeto

---
+++ README.md
@@ -0,0 +1,2 @@
+José Felipe de Castro Martins  - 22.120.029-8
+Pedro Henrique Guilherme Sanchez - 221200694
---
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -0,0 +1,93 @@
+package br.calebe.ticketmachine.core;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+class Troco {
+
+    protected PapelMoeda[] papeisMoeda;
+
+    public Troco(int valor) {
+        papeisMoeda = new PapelMoeda[6];
+        int count = 0;
+        while (valor > 100 && valor % 100 >= 0) {
+        count++;
+        valor -= 100;
+        }
+        papeisMoeda[5] = new PapelMoeda(100, count);
+        count = 0;
+        while (valor > 50 && valor % 50 >= 0) {
+        count++;
+        valor -= 50;
+        }
+        papeisMoeda[4] = new PapelMoeda(50, count);
+        count = 0;
+        while (valor > 20 && valor % 20 >= 0) {
+        count++;
+        valor -= 20;
+        }
+        papeisMoeda[3] = new PapelMoeda(20, count);
+        count = 0;
+        while (valor > 10 && valor % 10 >= 0) {
+        count++;
+        valor -= 10;
+        }
+        papeisMoeda[2] = new PapelMoeda(10, count);
+        count = 0;
+        while (valor > 5 && valor % 5 >= 0) {
+        count++;
+        valor -= 5;
+        }
+        papeisMoeda[1] = new PapelMoeda(5, count);
+        count = 0;
+        while (valor > 2 && valor % 2 >= 0) {
+        count++;
+        valor -= 2;
+        }
+        papeisMoeda[0] = new PapelMoeda(2, count);
+        count = 0;
+    }
+
+    public Iterator<PapelMoeda> getIterator() {
+        return new TrocoIterator(this);
+    }
+
+    class TrocoIterator implements Iterator<PapelMoeda> {
+
+        protected Troco troco;
+
+        public TrocoIterator(Troco troco) {
+            this.troco = troco;
+        }
+
+        @Override
+        public boolean hasNext() {
+            for (int i = 5; i >= 0; i--) {
+                if (troco.papeisMoeda[i] != null) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public PapelMoeda next() {
+            PapelMoeda ret = null;
+            for (int i = 5; i >= 0 && ret != null; i--) {
+                if (troco.papeisMoeda[i] != null) {
+                    ret = troco.papeisMoeda[i];
+                    troco.papeisMoeda[i] = null;
+                }
+            }
+            return ret;
+        }
+
+        @Override
+        public void remove() {
+            next();
+        }
+    }
+}
